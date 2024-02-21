48e0db3e770f9b43fb1c1c499acb28268f95453d
kot2d <kot2d@outlook.com>
2024-02-21 15:26:17 +0100

    добавил модель с юрл для яндекс oauth

--- src/main/java/com/kivanov/diploma/contollers/ProjectController.java
+++ src/main/java/com/kivanov/diploma/contollers/ProjectController.java
@@ -1,15 +1,23 @@
 package com.kivanov.diploma.contollers;
 
+import com.kivanov.diploma.model.WebUrls;
+import com.kivanov.diploma.services.YandexService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/project")
+@RequestMapping("/" + WebUrls.PROJECT)
 public class ProjectController {
 
-    @GetMapping("/new")
-    public String home() {
+    @Autowired
+    YandexService yandexService;
+
+    @GetMapping("/" + WebUrls.NEW)
+    public String addNewProject(Model model) {
+        model.addAttribute("yandexOauthUrl", yandexService.getAuthorizationUrl());
         return "new-project";
     }
 }
