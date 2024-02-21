3ebc20cec461ddc06013520903d7352666c23ab7
kot2d <kot2d@outlook.com>
2024-02-21 19:59:59 +0100

    исправил контролле добавил аннотацию @Controller

--- src/main/java/com/kivanov/diploma/contollers/OauthController.java
+++ src/main/java/com/kivanov/diploma/contollers/OauthController.java
@@ -1,14 +1,29 @@
 package com.kivanov.diploma.contollers;
 
 import com.kivanov.diploma.model.WebUrls;
+import com.kivanov.diploma.services.YandexService;
+import com.kivanov.diploma.services.YandexUserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+@Controller
 @RequestMapping("/" + WebUrls.OAUTH2)
 public class OauthController {
 
-    @GetMapping("/" + WebUrls.YANDEX)
-    public void getAuthCodeFromYandex() {
+    @Autowired
+    YandexService yandexService;
 
+    @GetMapping("/" + WebUrls.YANDEX)
+    public String getAuthCodeFromYandex(@RequestParam("code") String code, Model model) throws IOException, ExecutionException, InterruptedException {
+        yandexService.doOauth(code);
+        model.addAttribute("oauth", code);
+        return "new-project";
     }
 }
