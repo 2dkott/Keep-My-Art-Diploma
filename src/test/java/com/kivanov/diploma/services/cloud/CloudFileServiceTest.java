c2b41d1674db6140b2674fbf74b85cbc29713a56
kot2d <kot2d@outlook.com>
2024-03-14 15:24:43 +0100

    добавил тесты на облачный сервис

--- src/test/java/com/kivanov/diploma/services/cloud/CloudFileServiceTest.java
+++ src/test/java/com/kivanov/diploma/services/cloud/CloudFileServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
